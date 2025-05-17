f94f5b77d5bca86aedba472bef3dbf27446c24a7
professorfilipo <filipo.mor@gmail.com>
2025-05-17 11:14:25 -0300

    popula o tabuleiro com a quantidade configurada de bombas, garantindo que nao existam posicoes repetidas

--- cacaminas_parte01.py
+++ cacaminas_parte01.py
@@ -1,9 +1,10 @@
 ###
 ### Caca Minas - parte 01
 ###
+import random as rd
 
 ### Configuracoes
-qtBombas    = 4
+qtBombas    = 5
 qtBandeiras = 0
 Largura     = 5 # qt de colunas do tabuleiro
 Altura      = 5 # qt de linhas do tabuleiro
@@ -28,5 +29,24 @@ def MostraTabuleiro(tabuleiro):
     for x in range(0, len(tabuleiro)):
         print(tabuleiro[x])
 
+###
+### Popula o tabuleiro com a quantidade de bombas configurada
+###
+def CriaBombas(tabuleiro, qtBombas, valorBomba):
+    cont = 0
+    for x in range(0, qtBombas):
+        posLivre = False
+        while not posLivre:
+            posLivre = True
+            posX = rd.randint(0, Largura-1)
+            posY = rd.randint(0, Altura-1)
+            if tabuleiro[posX][posY] == valorBomba:
+                posLivre = False
+            print(posX, posY)
+            cont += 1
+        tabuleiro[posX][posY] = valorBomba
+        print(f"sorteios: {cont}")
+
 CriaTabuleiro(Tabuleiro, Altura, Largura, 0)
+CriaBombas(Tabuleiro, qtBombas, valorBomba)
 MostraTabuleiro(Tabuleiro)
